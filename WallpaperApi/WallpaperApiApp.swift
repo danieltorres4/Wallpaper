9ed4524194936c347f0b345307166473139bf050
danieltorres4 <dst990502@outlook.com>
2023-03-02 11:30:22 -0600

    Initial Commit

---
+++ WallpaperApi/WallpaperApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperApiApp.swift
+//  WallpaperApi
+//
+//  Created by Iván Sánchez Torres on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
